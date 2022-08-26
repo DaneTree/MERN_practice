789bc81815b60cffa6050d81d33e412a9c7c8096
Dane Goodman <dane@treelineinteractive.com>
2022-08-26 16:34:04 -0700

    cleaning up code

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express(); 
+const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 
@@ -8,20 +9,19 @@ const lessons = [
     {id: 2, lesson: 2}, 
     {id: 3, lesson: 3}
 ];
-
-app.get('/', function(req, res) {
-    res.send('Hello World');
+//GET Route
+app.get('/', (req, res) => res.send('Hello World'));
+//GET Route
+app.get('/api/lessons', (req, res) => res.send([lessons]));
+//GET return specified route
+app.get('/api/lessons/:id', (req, res) => {
+    const lesson = lessons.find( l => l.id === parseInt(req.params.id)); 
+    if(!lesson) res.status(404).send('The lesson ID given was not found')
+    res.send(lesson)
 });
-
-//Get Post Put Delete
-app.get('/api/lessons', function(req, res) {
-    res.send([lessons]); 
-});
-
-app.post('/api/lessons', function(req, res) {
-    if(!req.body.lesson || req.body.lesson.length < 3) {
-        res.status(400).send('lesson required and should be at least 3 characters long')
-    }
+//POST Route
+app.post('/api/lessons', (req, res) => {
+    if(!req.body.lesson || req.body.lesson.length < 3) res.status(400).send('lesson required and should be at least 3 characters long')
     const lesson = {
         id: lessons.length + 1,
         lesson: req.body.lesson
@@ -29,17 +29,9 @@ app.post('/api/lessons', function(req, res) {
     lessons.push(lesson)
     res.send(lesson);
 });
-
-app.put('/api/lessons/:id', function(req, res) {
-    //Look up existing lessons
-    //If lesson do not exist, return a 404 error - not found
-    //validate the input
-    //If input is invalid, then return a 400 error - bad request 
-    //update the specified lesson
-    //Return the updated lesson to the client, in the browser
-    const lesson = lessons.find(function(l) {
-        return l.id === parseInt(req.params.id)
-    }); 
+//PUT Route
+app.put('/api/lessons/:id', (req, res) => {
+    const lesson = lessons.find(l =>  l.id === parseInt(req.params.id)); 
     if(!lesson) {
         res.status(404).send('The lesson ID given was not found')
     }
@@ -49,46 +41,17 @@ app.put('/api/lessons/:id', function(req, res) {
     if(req.body.lesson || req.body.lesson.length < 3) {
         res.status(200).send('lesson updated successfully')
     }
-
     lesson.lesson = req.body.lesson
 })
-
-app.delete('/api/lessons/:id', function(req, res) {
-    //look up all of the lessons
-    //If it does not exist, return a 404
-    //delete
-    //return lesson that was deleted 
-    const lesson = lessons.find(function(l) {
-        return l.id === parseInt(req.params.id)
-    }); 
-    if(!lesson) {
-        res.status(404).send('The lesson ID given was not found')
-    }
+//DELETE Route
+app.delete('/api/lessons/:id', (req, res) => {
+    const lesson = lessons.find(l => l.id === parseInt(req.params.id)); 
+    if(!lesson) res.status(404).send('The lesson ID given was not found')
     const index = lessons.indexOf(lesson)
     lessons.splice(index, 1)
     res.send(lesson);
 });
-
-//End Get Put Post Delete 
-
-
-//return specified route
-app.get('/api/lessons/:id', function(req, res) {
-    const lesson = lessons.find(function(l) {
-        return l.id === parseInt(req.params.id)
-    }); 
-    if(!lesson) {
-        res.status(404).send('The lesson ID given was not found')
-    }
-    res.send(lesson)
-});
-
 //return the paramaters
-app.get('/api/lessons/:year/:title', function(req, res) {
-    res.send(req.params);
-});
+app.get('/api/lessons/:year/:title', (req, res) => res.send(req.params));
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function() {
-    console.log(`app is running on port: ${PORT}`);
-});
+app.listen(PORT,() => console.log(`app is running on port: ${PORT}`));
